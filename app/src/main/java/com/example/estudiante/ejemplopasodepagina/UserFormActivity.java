02506952eef8a987113f43ebb4ba9099dde0e337
Paula P <paula_ale_pe@hotmail.com>
2018-08-23 12:59:35 -0500

    Avance de mitad de clase

--- app/src/main/java/com/example/estudiante/ejemplopasodepagina/UserFormActivity.java
+++ app/src/main/java/com/example/estudiante/ejemplopasodepagina/UserFormActivity.java
@@ -3,6 +3,7 @@ package com.example.estudiante.ejemplopasodepagina;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,6 +29,27 @@ public class UserFormActivity extends AppCompatActivity {
         edtUserFormNick = findViewById(R.id.edt_userform_nick);
 
         btnUserFormFinish = findViewById(R.id.btn_userform_finish);
+
+        btnUserFormFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent respuesta = new Intent();
+
+                String name = edtUserFormName.getText().toString();
+                String email = edtUserFormEmail.getText().toString();
+                String nick = edtUserFormNick.getText().toString();
+
+                respuesta.putExtra("name",name);
+                respuesta.putExtra("email",email);
+                respuesta.putExtra("nick",nick);
+
+                setResult(Constants.RESP_NEW_USER, respuesta);
+                finish();
+
+
+
+            }
+        });
 
 
     }
