cb13f29a68e1e8348a2cb18874ce7e5d49f5c6d3
Paula P <paula_ale_pe@hotmail.com>
2018-08-23 12:24:51 -0500

    Avance de mitad de clase

--- app/src/main/java/com/example/estudiante/ejemplopasodepagina/MainActivity.java
+++ app/src/main/java/com/example/estudiante/ejemplopasodepagina/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.estudiante.ejemplopasodepagina;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.lang.invoke.ConstantCallSite;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +29,21 @@ public class MainActivity extends AppCompatActivity {
         btnMainToForm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent solicitud = new Intent(getApplicationContext(),
+                UserFormActivity.class);
+
+                startActivityForResult(solicitud, Constants.REQ_NEW_USER);
+
             }
         });
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
+
